93372f4eb0439cec19e50de1aa7a8a461cd7f82c
marianocrowe <merunga@gmail.com>
2013-10-16 17:34:57 -0500

    documentacion del proceso

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-ejemplo-scraping-datosperu
-==========================
+Ejemplo de como scrapear [datosperu.org](datosperu.org)
+========================================
 
-Codigo de ejemplo para el scrapeo de datosperu.org usando harvestma
+Para ejecutar este ejemplo, necesitarás una instalación de ruby con
+[harvestman](https://github.com/mion/harvestman) instalado y
+una conexión a internet.
+
+Para ejecutar el crawler sólo tienes que ejecutar
+```
+ruby  crawl.rb
+```
+
+Este script visitará las primeras 3 páginas del rubro
+[Otras Asociaciones](datosperu.org/ep-otras-asociaciones-ncp-91993-pagina-1.php)
+en [datosperu.org](datosperu.org) y extraerá el nombre de la organización y su RUC.
+
+Para entender mejor cómo se hace el scrapero, chequea el script `crawl.rb`, que tiene varios comentarios
+Algunos resultados podrás encontrarlos en el archivo `resultados_crawler.txt`
+
+Y por último para poder adaptar este script a tus necesidades, es recomendado que le eches
+un vistazo a la tecnología [XPATH](http://www.w3schools.com/xpath/)
---
+++ crawl.rb
@@ -0,0 +1,25 @@
+require 'harvestman'
+
+base_url = "http://www.datosperu.org/" # papina principal
+crawl_url = base_url+"ep-otras-asociaciones-ncp-91993-pagina-*.php" #plantilla se paginas a visitas
+
+Harvestman.crawl crawl_url, (1..2) do # vamos a visitar solo las paginas 1 y 2:
+# http://datosperu.org/ep-otras-asociaciones-ncp-91993-pagina-1.php
+# http://datosperu.org/ep-otras-asociaciones-ncp-91993-pagina-2.php
+  xpath "//nav/ol/li/a"  do |it|
+# para cada link de una empresa (estos links estan en el atributo href de los
+# tags a dentro de nav > ol > li)
+    inner_crawl_url = base_url + (it.xpath '@href')
+    Harvestman.crawl inner_crawl_url do # un nuevo crawl al link encontrado
+      begin
+      	puts "Nombre:" +( xpath "//header/h1" ) # el nombre de la empresa está en el tag H1
+      	puts "RUC:" +( xpath "//dl[3]/dd" ) # el duc está en el cuarto DL dentro del tag DD
+      rescue
+        puts "***** Error con url: "+ inner_crawl_url # Ooops! o la data no está u otra cosa paso
+      ensure
+        puts '-------------'
+      end
+    end
+  end
+end
+
